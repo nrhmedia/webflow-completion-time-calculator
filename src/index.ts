6eb591c747b01b8483832af5bfc7eeb923c7c7e2
nrhmedia <git@nrhmedia.com>
2024-05-14 13:30:55 +0700

    First Commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,32 @@
-import { greetUser } from '$utils/greet';
+$("[data-post-wrapper]").each(function () {
+  let link = $(this).find("[data-post-link]").attr("href");
+  let minutesText = $(this).find("[data-read-time]");
+  $.ajax({
+    url: link,
+    success: function (response) {
+      let cmsPage = $(response);
+      let richTextElements = cmsPage.find('[count-me="true"]');
+      const wordsPerMinute = 250;
+      const secondsPerImage = 10;
 
-window.Webflow ||= [];
-window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+      let totalWords = 0;
+      let totalImages = 0;
+
+      richTextElements.each(function () {
+        totalWords += $(this).text().split(" ").length;
+        totalImages += $(this).find("img").length;
+      });
+
+      const totalMinutes = Math.floor((totalWords / wordsPerMinute) + (totalImages * secondsPerImage / 60));
+      const totalSeconds = (totalWords / wordsPerMinute) * 60 + (totalImages * secondsPerImage);
+
+      if (totalSeconds < 60) {
+        minutesText.text(`Less than 1 minute`);
+      } else if (totalMinutes === 1) {
+        minutesText.text(`1 minute`);
+      } else {
+        minutesText.text(`${totalMinutes} minutes`);
+      }
+    }
+  });
 });
